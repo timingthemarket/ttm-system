e7a8e9b98d5a9211dd9f56d33d9e934accd5a205
Eric H <137773961+herwineric-EC@users.noreply.github.com>
2026-06-13 11:36:15 +0200

    docker build and deploy

---
+++ article-news-raw/docker-push.sh
@@ -0,0 +1,12 @@
+IMAGE_NAME="article-news-raw"
+
+echo "Building $IMAGE_NAME..."
+docker build -f Dockerfile -t $IMAGE_NAME:latest ..
+
+echo "Tagging $IMAGE_NAME..."
+docker tag $IMAGE_NAME:latest 192.168.68.63:5000/$IMAGE_NAME:latest
+
+echo "Pushing $IMAGE_NAME..."
+docker push 192.168.68.63:5000/$IMAGE_NAME:latest
+
+echo "Done."
