f38a7cb28db003c0e7cb310e7f5d99ba89054228
herwi <herwineric@gmail.com>
2026-07-11 10:55:29 +0000

    docker changes

--- riksbanken-raw/docker-push-local.sh
+++ riksbanken-raw/docker-push-local.sh
@@ -4,9 +4,9 @@ echo "Building $IMAGE_NAME..."
 docker build -f Dockerfile -t $IMAGE_NAME:latest ..
 
 echo "Tagging $IMAGE_NAME..."
-docker tag $IMAGE_NAME:latest localhost:5000/$IMAGE_NAME:latest
+docker tag $IMAGE_NAME:latest 127.0.0.1:5000/$IMAGE_NAME:latest
 
 echo "Pushing $IMAGE_NAME..."
-docker push localhost:5000/$IMAGE_NAME:latest
+docker push 127.0.0.1:5000/$IMAGE_NAME:latest
 
 echo "Done."
